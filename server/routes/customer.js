b1c60ff047ce6120f15f6422aea35c1f40c8a26c
Ahmad Mash <wolfmashagbah@hotmail.com>
2024-02-06 23:03:29 +0300

    Tests succeeded for customer controller

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 const customerController = require("../controllers/customer");
 
+router.get('/', customerController.getAllCustomers)
+router.post('/', customerController.addCustomer)
 router.get("/filter", customerController.filterShopItems);
 router.get("/search", customerController.searchShopItems);
+router.get('/:customerId/cart', customerController.getCart )
 router.post("/:customerId/cart", customerController.addToCart);
 router.post("/:customerId/checkout", customerController.checkout);
 router.get("/:id", customerController.getSingleItem)
