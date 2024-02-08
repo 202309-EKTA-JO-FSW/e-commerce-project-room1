2ef3ca0b89ec55528fe50e3c6844ef83bac0ac51
Ahmad Mash <wolfmashagbah@hotmail.com>
2024-02-08 16:18:36 +0300

    final changes

--- server/middlewares/verification.js
+++ server/middlewares/verification.js
@@ -1,16 +1,17 @@
 const jwt = require('jsonwebtoken');
 
-module.exports = (redirect = '/customers/signin') => (req, res, next) => {
+module.exports = (req, res, next) => {
   const token = req.headers.authorization;
+  redirect = '/customers/signin'
 
   if (!token) {
-    if (req.method === 'GET') res.redirect(redirect);
+    if (req.method === 'GET') res.status(403).json('redirecting user to signIn');
     else res.status(403).end();
   } else {
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
       if (err) {
-        if (req.method === 'GET') res.redirect(redirect);
-        else res.status(403).end();
+        if (req.method === 'GET') res.status(403).json('redirecting user to signIn')
+        else res.status(403).json({ message: "Authentication failed"}).end();
       } else {
         req.user = user;
         next();
